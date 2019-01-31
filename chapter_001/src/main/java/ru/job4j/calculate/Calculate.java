afe516f2926dc8f69155378634c25bcba7028f95
Vladislav-Baikov <vagrant73@gmail.com>
2019-01-31 17:11:50 +0300

    info

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j.calculate;
 
+/**
+* Package for calculate task.
+*
+* @author Vladislav Baikov (vagrant73@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
 public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Hello World");
