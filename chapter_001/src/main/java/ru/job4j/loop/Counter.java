e49d60498d96f8699281347e617169883b00f5eb
Vladislav-Baikov <vagrant73@gmail.com>
2019-02-21 16:35:45 +0300

    4.4. Максимум из трех чисел[#106755]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+
+/**
+ * @author Vladislav Baikov (vagrant73@gmail.com)
+ * @version v0.1
+ * @since 21.02.19
+ */
+
+
+public class Counter {
+    /**
+     * Считает сумму четных элементов.
+     * @param start, finish Определение диапазона чисел.
+     * @return summ Сумма четных элементов.
+     */
+    public int add(int start, int finish) {
+        int summ = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                summ += i;
+            }
+        }
+        return summ;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Vladislav Baikov (vagrant73@gmail.com)
+ * @version v0.1
+ * @since 21.02.19
+ */
+
+public class CounterTest {
+    /**
+     * Тест диапазон 1 - 10, сумма четных элементов 30.
+     */
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        Counter counter = new Counter();
+        int result = counter.add(1, 10);
+        assertThat(result, is(30));
+    }
+}
