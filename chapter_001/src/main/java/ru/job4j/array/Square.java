441041eccc288e8de46381c6f43ff36cecdfe897
Vladislav-Baikov <vagrant73@gmail.com>
2019-02-25 17:31:56 +0300

    6.0. Заполнить массив степенями чисел.[#106766]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+/**
+ * @author Vladislav Baikov (vagrant73@gmail.com)
+ * @version v0.1
+ * @since 25.02.19
+ */
+
+public class Square {
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+            for (int i = 0; i != bound ; i++) {
+                rst[i] = (int) Math.pow((i + 1), 2);
+            }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Vladislav Baikov (vagrant73@gmail.com)
+ * @version v0.1
+ * @since 25.02.19
+ */
+public class SquareTest {
+    /**
+     * Тест записи квадратов индексов при 3 значениях.
+     */
+    @Test
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+    /**
+     * Тест записи квадратов индексов при 4 значениях.
+     */
+    @Test
+    public void whenBound4Then14916() {
+        int bound = 4;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9, 16};
+        assertThat(rst, is(expect));
+    }
+    /**
+     * Тест записи квадратов индексов при 5 значениях.
+     */
+    @Test
+    public void whenBound5Then1491625() {
+        int bound = 5;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9, 16, 25};
+        assertThat(rst, is(expect));
+    }
+
+
+}
