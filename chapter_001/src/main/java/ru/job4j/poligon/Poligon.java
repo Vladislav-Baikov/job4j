cb9afad965479135ae9b1eba2cdf779737de2009
Vladislav-Baikov <vagrant73@gmail.com>
2019-02-27 16:12:29 +0300

    My tests

---
+++ chapter_001/src/main/java/ru/job4j/poligon/Poligon.java
@@ -0,0 +1,8 @@
+package ru.job4j.poligon;
+
+public class Poligon {
+    public static void main(String[] args) {
+        int[]array = new int[10];
+        System.out.println(array.length);
+    }
+}
