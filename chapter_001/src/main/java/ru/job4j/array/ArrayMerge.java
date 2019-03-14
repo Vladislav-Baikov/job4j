cc1acb8e92de814d5e7ba8d8e6e332f23c387a08
Vladislav-Baikov <vagrant73@gmail.com>
2019-03-14 15:56:00 +0300

    Экзамен. Базовый синтаксис.[#106796]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayMerge.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+/**
+ * @author Vladislav Baikov (vagrant73@gmail.com)
+ * @version v0.1
+ * @since 14.03.19
+ */
+public class ArrayMerge {
+    public int[] Merge(int[] first, int[] second) {
+        int[] array = new int[first.length + second.length];
+        int markerFirst = 0;
+        int markerSecond = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (first[markerFirst] < second[markerSecond]) {
+                array[i] = first[markerFirst];
+                array[i + 1] = second[markerSecond];
+                markerFirst++;
+            } else {
+                array[i] = second[markerSecond];
+                array[i + 1] = first[markerFirst];
+                markerSecond++;
+            }
+        }
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayMergeTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Vladislav Baikov (vagrant73@gmail.com)
+ * @version v0.1
+ * @since 14.03.19
+ */
+public class ArrayMergeTest {
+    /**
+    * Слияние 2х отсортированных массивов
+     **/
+    @Test
+    public void createMergedArrayFromTwoArrays() {
+        ArrayMerge test = new ArrayMerge();
+        int[] first = new int[]{1, 3, 5};
+        int[] second = new int[]{2, 4};
+        int[] result = test.Merge(first, second);
+        int[] expect = new int[]{1, 2, 3, 4, 5};
+        assertThat(result, Matchers.is(expect));
+    }
+}
